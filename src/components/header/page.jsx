5adb34ea666e98ffea9b4abd9f8c1961eb3ea707
jolypo <koolase783@gmail.com>
2025-12-07 01:02:05 +0300

    Update page.jsx

--- src/components/header/page.jsx
+++ src/components/header/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import Sidebar from "../Sidebar/page";
+import Sidebar from "../sidebar/page";
 import "./header.css";
 
 const Header = () => {
